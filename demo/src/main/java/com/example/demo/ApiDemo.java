c462c8bb687f63b2b6b427b6308c53de8eb86079
Amit Kumar <99895289+AmitKumar001111@users.noreply.github.com>
2022-07-13 14:30:44 +0530

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/ApiDemo.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+public class ApiDemo {
+
+	public ApiDemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
